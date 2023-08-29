f62cba6251afdbc304857ca91fa64d9e38a9baf5
yugkhokhar <yugkhokhar18@gmail.com>
2023-08-29 18:59:30 +0530

    second test case passed

--- test.cpp
+++ test.cpp
@@ -12,3 +12,15 @@ TEST_CASE("Test Case 1") {
     REQUIRE(chandrayan3.getCurrentDirection() == "N");
 }
 
+//TEST CASE FOR GSLV3_MOVE_BACKWARD
+
+TEST_CASE("Test Case 2") {
+    vector<int> pos = {0, 1, 0};
+    string dir = "N";
+    Chandrayan3 chandrayan3(pos, dir);
+    chandrayan3.GSLV3_MOVE_RIGHT();
+    REQUIRE(chandrayan3.getCurrentPositions() == vector<int>{0, 1, 0});
+    REQUIRE(chandrayan3.getCurrentDirection() == "E");
+}
+
+
